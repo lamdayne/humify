632922bd9ab891a75b1920014d0a9a37a238b53f
lamdayne <nlam32428@gmail.com>
2026-05-22 14:50:42 +0700

    init project

---
+++ src/main/java/com/lamdayne/humify/HumifyApplication.java
@@ -0,0 +1,13 @@
+package com.lamdayne.humify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lamdayne/humify/HumifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamdayne.humify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
