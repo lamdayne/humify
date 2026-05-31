336bd09572cf69d5b16c2e4eef78c266462af72f
lamdayne <nlam32428@gmail.com>
2026-05-31 23:17:10 +0700

    feat(permission): add permission for system

---
+++ src/main/java/com/lamdayne/humify/auth/enums/PermissionModule.java
@@ -0,0 +1,17 @@
+package com.lamdayne.humify.auth.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PermissionModule {
+    USER,
+    EMPLOYEE,
+    COMPANY,
+    BRANCH,
+    DEPARTMENT,
+    POSITION,
+    ATTENDANCE,
+    ROLE,
+    PERMISSION,
+    SYSTEM
+}
