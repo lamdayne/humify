214b929412989c363f708b6d8808aa5e830a522c
lamdayne <nlam32428@gmail.com>
2026-05-31 23:22:07 +0700

    feat(role_has_permission): add method findByRoleId

---
+++ src/main/java/com/lamdayne/humify/auth/repository/RoleHasPermissionRepository.java
@@ -0,0 +1,17 @@
+package com.lamdayne.humify.auth.repository;
+
+import com.lamdayne.humify.auth.entity.RoleHasPermission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleHasPermissionRepository extends JpaRepository<RoleHasPermission,Long> {
+
+    @Query("select rhp from RoleHasPermission rhp join fetch rhp.permission where rhp.role.id = :roleId")
+    List<RoleHasPermission> findByRoleId(@Param("roleId") Long roleId);
+
+}
