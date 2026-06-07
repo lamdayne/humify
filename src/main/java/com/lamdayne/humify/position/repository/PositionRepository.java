af203d8f02147f3a1fd12c26a7542f3a9250c99b
lamdayne <nlam32428@gmail.com>
2026-06-07 17:35:40 +0700

    refactor(position): update API get all position

--- src/main/java/com/lamdayne/humify/position/repository/PositionRepository.java
+++ src/main/java/com/lamdayne/humify/position/repository/PositionRepository.java
@@ -1,12 +1,10 @@
 package com.lamdayne.humify.position.repository;
 
 import com.lamdayne.humify.position.entity.Position;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
-    Page<Position> findByCompanyId(Long companyId, Pageable pageable);
+
 }
