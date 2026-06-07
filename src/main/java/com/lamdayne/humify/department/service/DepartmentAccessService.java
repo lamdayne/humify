1c27aa3e247a498bdc1ae3e447743255c4dfa73b
lamdayne <nlam32428@gmail.com>
2026-06-07 17:39:16 +0700

    feat(department): impl DepartmentAccessService

---
+++ src/main/java/com/lamdayne/humify/department/service/DepartmentAccessService.java
@@ -0,0 +1,7 @@
+package com.lamdayne.humify.department.service;
+
+import com.lamdayne.humify.common.base.BaseAccessService;
+import com.lamdayne.humify.department.entity.Department;
+
+public interface DepartmentAccessService extends BaseAccessService<Department, Long> {
+}
