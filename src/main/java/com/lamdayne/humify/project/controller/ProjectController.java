a8c40f332da9f9a7e8554e0a697682fbff8e535b
lamdayne <nlam32428@gmail.com>
2026-06-24 11:06:17 +0700

    feat(project): add endpoint project

---
+++ src/main/java/com/lamdayne/humify/project/controller/ProjectController.java
@@ -0,0 +1,11 @@
+package com.lamdayne.humify.project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/projects")
+public class ProjectController {
+}
