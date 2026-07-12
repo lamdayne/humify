285817b3de764bc7a81bc21dc806308fc9e02fe6
lamdayne <nlam32428@gmail.com>
2026-07-12 09:06:11 +0700

    feat(attendance): add repository for attendance

---
+++ src/main/java/com/lamdayne/humify/attendance/repository/WorkShiftRepository.java
@@ -0,0 +1,9 @@
+package com.lamdayne.humify.attendance.repository;
+
+import com.lamdayne.humify.attendance.entity.WorkShift;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkShiftRepository extends JpaRepository<WorkShift, Long> {
+}
