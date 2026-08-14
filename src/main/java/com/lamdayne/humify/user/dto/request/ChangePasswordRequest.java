3eaf241015baddd57c4d012f63c656dfad2fc704
lamdayne <nlam32428@gmail.com>
2026-08-14 22:49:53 +0700

    test: update test

--- src/main/java/com/lamdayne/humify/user/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/lamdayne/humify/user/dto/request/ChangePasswordRequest.java
@@ -1,9 +1,13 @@
 package com.lamdayne.humify.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordRequest {
 
     @NotBlank(message = "USER_OLD_PASSWORD_REQUIRED")
@@ -13,3 +17,4 @@ public class ChangePasswordRequest {
     private String newPassword;
 
 }
+
