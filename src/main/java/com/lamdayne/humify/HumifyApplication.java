328702e4288f04534ebd30c949395a7a6b8972cf
lamdayne <nlam32428@gmail.com>
2026-08-31 20:45:57 +0700

    refactor: update permission for role

--- src/main/java/com/lamdayne/humify/HumifyApplication.java
+++ src/main/java/com/lamdayne/humify/HumifyApplication.java
@@ -3,11 +3,13 @@ package com.lamdayne.humify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class HumifyApplication {
 
     public static void main(String[] args) {
